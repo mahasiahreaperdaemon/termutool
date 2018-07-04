b69ab6975a3bfbaab7e8a865cdacbf8b33d65f27
mahasiahreaperdaemon <33765885+mahasiahreaperdaemon@users.noreply.github.com>
2018-07-04 14:04:05 -0300

    Add files via upload

---
+++ termutool.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+import time
+import sys
+
+def principal():
+    print("#=#=#=#=#=#=#=#=#=#")
+    print("     TERMUTOOL     ")
+    print("#=#=#=#=#=#=#=#=#=#")
+    print("Criado por: Astaroth Ariel")
+    print("--------------------------")
+          
+
+
+          
+principal()
