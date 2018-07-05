8d0eeb70788f9547e8efecdb54e3cc47fe4edf2a
mahasiahreaperdaemon <33765885+mahasiahreaperdaemon@users.noreply.github.com>
2018-07-05 17:33:38 -0300

    Add files via upload

---
+++ instalacoes.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+import time
+
+
+def aribanner():
+    print("Baixando AriBanner...")
+    time.sleep(3)
+    os.system("git clone https://github.com/mahasiahreaperdaemon/captura_banner.git")
+    print("=========================")
+    print("CONCLUIDO!!!")
+    print("Digite 'cd captura_banner' para entrar na pasta do programa")
+    print("Em seguida digite 'python3 ariBanner.py [alvo.com.br] [porta]' para executar o programa")
+    
+
+def ariport():
+    print("Baixando AriPort...")
+    time.sleep(3)
+    os.system("git clone https://github.com/mahasiahreaperdaemon/ariport.git")
+    print("=========================")
+    print("CONCLUIDO!!!")
+    print("Digite 'cd ariport' para entrar na pasta do programa")
+    print("Em seguida digite 'python ariport.py' para executar o programa")
+    
+    
+def exibirMensagem():
+    print("Obrigado por usar o TermuTool... Tchau! :D")
+    
+    
+def termutoolMsg():
+    print("\033[32m#=#=#=#=#=#=#=#=#=#")
+    print("     TERMUTOOL     ")
+    print("#=#=#=#=#=#=#=#=#=#")
+    print("Criado por: \033[35mAstaroth Ariel\033[m")
+    print("--------------------------\033[m")
+    
+    
+    
+def portscanMsg():
+    print("\033[32m#=#=#=#=#=#=#=#=#=#")
+    print("     TERMUTOOL     ")
+    print("-------------------")
+    print("  \033[36mScanner de portas\033[m")
+    print("\033[32m#=#=#=#=#=#=#=#=#=#\033[m")
+                
+                
+                
+def bannerCapMsg():
+    print("\033[32m#=#=#=#=#=#=#=#=#=#")
+    print("     TERMUTOOL     ")
+    print("-------------------")
+    print("  \033[36mCaptura de Banner\033[m")
+    print("\033[32m#=#=#=#=#=#=#=#=#=#\033[m")
